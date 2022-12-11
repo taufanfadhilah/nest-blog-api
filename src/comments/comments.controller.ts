bcb480c673eb7b6e2bb41155d222b7c8bd9f9e98
Taufan Fadhilah <taufanfadhilahiskandar@gmail.com>
2022-12-11 10:32:17 +0700

    fix: use HttpStatus for status code

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Delete, Param, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
@@ -12,13 +20,13 @@ export class CommentsController {
     const comment = this.commentsService.create(createCommentDto);
 
     if (!comment) {
-      return res.status(404).json({
+      return res.status(HttpStatus.NOT_FOUND).json({
         success: false,
         message: 'Can not create comment, post not found',
       });
     }
 
-    return res.status(200).json({
+    return res.status(HttpStatus.OK).json({
       success: true,
       message: 'Comment created successfully',
       data: comment,
@@ -29,13 +37,13 @@ export class CommentsController {
   remove(@Param('id') id: string, @Res() res: Response) {
     const isDeleted = this.commentsService.remove(+id);
     if (!isDeleted) {
-      return res.status(404).json({
+      return res.status(HttpStatus.NOT_FOUND).json({
         success: false,
         message: 'Comment not found',
       });
     }
 
-    return res.status(200).json({
+    return res.status(HttpStatus.OK).json({
       success: true,
       message: 'Comment deleted successfully',
     });
